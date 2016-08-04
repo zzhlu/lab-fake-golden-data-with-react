9eec20839498386f5665f95edcc0715e339668d2
zhaolu <1632089314@qq.com>
2016-08-04 22:51:16 +0800

    finished

--- form.js
+++ form.js
@@ -1,75 +1,88 @@
 const App = React.createClass({
   getInitialState: function () {
     return {
-      isPreview: false,
+      isEditor: true,
       elements: []
     }
   },
   toggle: function () {
     this.setState({
-      isPreview: !this.state.isPreview
+      isEditor: !this.state.isEditor
     });
   },
   addElement: function (element) {
     const elements = this.state.elements;
     elements.push(element);
-    this.setState({
-      elements: elements
-    });
+    this.setState({elements});
   },
-  removeElement: function (index) {
+  deleteElement: function (index) {
     const elements = this.state.elements;
     elements.splice(index, 1);
-    this.setState({
-      elements: elements
-    });
+    this.setState({elements});
   },
   render: function () {
-    const text = this.state.isPreview ? '编辑' : '预览';
+    const isEditor = this.state.isEditor;
     return <div>
-      <button onClick={this.toggle}>{text}</button>
-      <Editor className={this.state.isPreview ? "hidden" : ""} onAdd={this.addElement} onRemove={this.removeElement} elements={this.state.elements}/>
-      <Preview className={this.state.isPreview ? "" : "hidden"} elements={this.state.elements}/>
-    </div>
+      <button onClick={this.toggle}>{isEditor ? "Preview" : "Edit"}</button>
+      <div className={isEditor ? "" : "hidden"}>
+        <Editor elements={this.state.elements} onAdd={this.addElement} onDelete={this.deleteElement}/>
+      </div>
+      <div className={isEditor ? "hidden" : ""}>
+        <Previewer elements={this.state.elements}/>
+      </div>
+    </div>;
   }
 });
 
 const Editor = React.createClass({
-  add: function () {
-    const element = $("input[name=elementType]:checked").val();
-    this.props.onAdd(element);
-  },
+  render: function () {
+    return <div>
+      <Left elements={this.props.elements} onDelete={this.props.onDelete}/>
+      <Right onAdd={this.props.onAdd}/>
+    </div>;
+  }
+});
+
+const Left = React.createClass({
   remove: function (index) {
-    this.props.onRemove(index);
+    this.props.onDelete(index);
   },
   render: function () {
-    const previewElements = this.props.elements.map((ele, index) => {
+    const elements = this.props.elements.map((ele, index) => {
       return <div key={index}>
         <input type={ele}/>
-        <button onClick={this.remove.bind(this, index)}> X</button>
+        <button onClick={this.remove.bind(this, index)}>X</button>
       </div>;
     });
-    return <div className={this.props.className}>
-      <div>{previewElements}</div>
-      <div>
-        <input type="radio" name="elementType" value="text"/>文本
-        <input type="radio" name="elementType" value="date"/>日期
-      </div>
-      <div>
-        <button onClick={this.add}> +</button>
-      </div>
+
+    return <div>
+      {elements}
     </div>
   }
 });
 
-const Preview = React.createClass({
+const Right = React.createClass({
+  add: function () {
+    const element = $("input[name=element]:checked").val();
+    this.props.onAdd(element);
+  },
+  render: function () {
+    return <div>
+      <input type="radio" name="element" value="text"/>Text
+      <input type="radio" name="element" value="date"/>Date
+      <button onClick={this.add}>+</button>
+    </div>
+  }
+});
+
+const Previewer = React.createClass({
   render: function () {
     const elements = this.props.elements.map((ele, index) => {
       return <div key={index}>
         <input type={ele}/>
       </div>;
     });
-    return <div className={this.props.className}>
+    return <div>
       {elements}
       <button>Submit</button>
     </div>;
